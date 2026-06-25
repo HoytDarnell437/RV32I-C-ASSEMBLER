abe4900f67ccf5d2c2b9de702cd60f8fe66c099e
Hoyt Darnell <hoytdarnell437@gmail.com>
2026-06-25 11:45:16 -0400

    create instruction.h and instruction.c

--- C/src/assembler.c
+++ C/src/assembler.c
@@ -4,6 +4,8 @@
 #include "../include/assembler.h"
 #include "../include/dynamic_array.h"
 #include "../include/table.h"
+#include "../include/instruction.h"
+
 
 // File macros
 #define MAX_LINE_LENGTH 512
@@ -19,18 +21,6 @@ typedef struct {
     char *data_image;
     char *instructions;
 } asm_t;
-
-// Enum for instruction type
-typedef enum { R, I, S, B, U, J } instr_type_t;
-
-// Struct for instructions
-typedef struct {
-    const char *name;
-    const instr_type_t type;
-    const int opcode;
-    const int funct3;
-    const int funct7;
-} instruction_t;
 
 // Core function prototypes
 static void read_assembly(asm_t *);
@@ -62,35 +52,6 @@ static const pair_t escape_table[] = {
     {NULL}
 };
 
-static const instruction_t instruction_table[] = {
-    // U-Type
-    {"lui", U, 0x37, -1, -1}, {"auipc", U, 0x17, -1, -1},
-    // J-Type
-    {"jal", J, 0x6f, -1, -1}, {"j",     J, 0x6f, -1, -1},
-    // B-Type
-    {"beq", B, 0x63, 0x0, -1}, {"bne",  B, 0x63, 0x1, -1}, {"blt",  B, 0x63, 0x4, -1},
-    {"bge", B, 0x63, 0x5, -1}, {"bltu", B, 0x63, 0x6, -1}, {"bgeu", B, 0x63, 0x7, -1},
-    // S-Type
-    {"sb",  S, 0x23, 0x0, -1}, {"sh", S, 0x23, 0x1, -1}, {"sw", S, 0x23, 0x2, -1},
-    // I-Type
-    {"jalr", I, 0x67, 0x0, -1},
-    {"lb",   I, 0x03, 0x0, -1}, {"lh",   I, 0x03, 0x1, -1}, {"lw",     I, 0x03, 0x2, -1},
-    {"lbu",  I, 0x03, 0x4, -1}, {"lhu",  I, 0x03, 0x5, -1},
-    {"addi", I, 0x13, 0x0, -1}, {"slti", I, 0x13, 0x2, -1}, {"sltiu",  I, 0x13, 0x3, -1},
-    {"xori", I, 0x13, 0x4, -1}, {"ori",  I, 0x13, 0x6, -1}, {"andi",   I, 0x13, 0x7, -1},
-    {"slli", I, 0x13, 0x1, 0x0}, {"srli", I, 0x13, 0x5, 0x0}, {"srai", I, 0x13, 0x5, 0x20},
-    // R-Type
-    {"add", R, 0x33, 0x0, 0x0}, {"sub",  R, 0x33, 0x0, 0x20}, {"sll", R, 0x33, 0x1, 0x0},
-    {"slt", R, 0x33, 0x2, 0x0}, {"sltu", R, 0x33, 0x3, 0x0}, {"xor",  R, 0x33, 0x4, 0x0},
-    {"srl", R, 0x33, 0x5, 0x0}, {"sra",  R, 0x33, 0x5, 0x20}, {"or",  R, 0x33, 0x6, 0x0},
-    {"and", R, 0x33, 0x7, 0x0},
-    // System & Memory Fences
-    {"fence", I, 0x0f, 0x0, -1}, {"fence.i", I, 0x0f, 0x1, -1},
-    {"ecall", I, 0x73, 0x0, -1}, {"ebreak",  I, 0x73, 0x0, -1},
-
-    {NULL}
-};
-
 // Main function
 int assemble(const char *filename){
     // Declare new asm_t variable and set its default values
@@ -98,7 +59,8 @@ int assemble(const char *filename){
     init_asm(&ctx, filename);
 
     // Read .asm file
-    read_assembly(&ctx);
+    //read_assembly(&ctx);
+    instruction_lookup(NULL);
     if (ctx.error){
         free_asm(&ctx);
         return ASM_FAILURE;
