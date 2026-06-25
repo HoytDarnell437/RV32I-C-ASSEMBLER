6bc5100d93bcc99facd45e833af1dade6a925a47
Hoyt Darnell <hoytdarnell437@gmail.com>
2026-06-25 12:19:17 -0400

    fix bug in instruction.c and add register.h and register.c

---
+++ C/include/register.h
@@ -0,0 +1,13 @@
+// Engineer: Hoyt Darnell
+// Institution: Georgia Institute of Technology
+// Last Updated: 06/25/2026
+// Usage: This library provides the functionality of lookup table for register values
+
+#ifndef REGISTER_H
+#define REGISTER_H
+
+
+// Public API functions
+
+
+#endif // TABLE_H
