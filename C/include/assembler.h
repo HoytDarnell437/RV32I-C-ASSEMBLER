56f1da496555bede89b4744f0fdd680438ec041a
Hoyt Darnell <hoytdarnell437@gmail.com>
2026-06-27 13:15:17 -0400

    removed redundant returns

--- C/include/assembler.h
+++ C/include/assembler.h
@@ -1,11 +1,13 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
-// status codes returned by assemble function
-#define ASM_SUCCESS 0
-#define ASM_FAILURE 1
-
-// main function takes in a filename and produces machine code and a data initialization image
+/**
+* @brief Read the .asm file provided and store it in a dynamic array of type array_t.
+*
+* Moves through the file appending each line to ctx->assembly.
+*
+* @param ctx Pointer to the active assembler context structure.
+*/
 int assemble(const char *);
 
 #endif // ASSEMBLER_H
