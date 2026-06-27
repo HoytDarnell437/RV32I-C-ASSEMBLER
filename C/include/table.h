95c9fb9e7426a8cbae20d3fe6d1ff756a7d83740
Hoyt Darnell <hoytdarnell437@gmail.com>
2026-06-27 18:22:31 -0400

    added support for doxygen in tabel.c and table.h

--- C/include/table.h
+++ C/include/table.h
@@ -1,27 +1,58 @@
-// Engineer: Hoyt Darnell
-// Institution: Georgia Institute of Technology
-// Last Updated: 06/25/2026
-// Usage: This library provides the functionality of a dynamically allocated linear lookup table consisting of pairs with a str key and int value
+/**
+ * @file table.h
+ * @author Hoyt Darnell
+ * @date 2026-06-27
+ * @brief Linear lookup table with string keys and integer values.
+ */
 
 #ifndef TABLE_H
 #define TABLE_H
 
-// Pair type
+#include <stdio.h>
+
 typedef struct {
     char *key;
     int value; 
 } pair_t;
 
-// Table type
 struct table_impl;
 typedef struct table_impl* table_t;
 
-// Public API functions
-table_t table_create(int initial_capacity); // Create and initialize table variable 
-void table_free(table_t table); // Deallocates the memory used by the table
-void table_print(table_t table, FILE *file); // Prints the contents of the table to file
+/**
+ * @brief Creates a table_t type.
+ * @param initial_capacity Initial capacity of created table.
+ * @return Returns a pointer to the created table.
+ */
+table_t table_create(int initial_capacity);
 
-void table_set(table_t table, const char *key, int value); // Adds a pair to the table or updates existing key to new value
-int table_get(const table_t table, const char *key, int *value); // Retrieves the value associated with the key
+/**
+ * @brief Frees all elements of a table_t type.
+ * @param table Pointer to table to free.
+ */
+void table_free(table_t table);
+
+/**
+ * @brief Prints the contents of a table.
+ * @param table Pointer to table object.
+ * @param file File to print contents to.
+ */
+void table_print(const table_t table, FILE *file);
+
+/**
+ * @brief Sets the value of key in the table.
+ * @param table Pointer to table_t object.
+ * @param key Key of table in which the value will be changed.
+ * @param value Value to change key to be associated with.
+ */
+void table_set(table_t table, const char *key, int value);
+
+/**
+ * @brief Retrieves the value of a table associated with key.
+ * @param table Pointer to table object.
+ * @param key Key to retrieve value from the table.
+ * @param value Pointer where the retrieved value will be stored.
+ * @return Returns 1 if key is found 0 if not
+ */
+int table_get(const table_t table, const char *key, int *value);
 
 #endif // TABLE_H
--- C/src/table.c
+++ C/src/table.c
@@ -1,109 +1,93 @@
-// Engineer: Hoyt Darnell
-// Institution: Georgia Institute of Technology
-// Implementation for table.h
+/**
+ * @file table.c
+ * @brief Implementation for table.h
+ */
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../include/table.h"
 
-// Struct for lookup table
-typedef struct table_impl{
+typedef struct table_impl {
     int capacity;
     int size;
     pair_t *pairs;
 } table_impl_t;
 
-// Function prototypes
 static pair_t pair_create(const char *key, int value);
-static void pair_print(pair_t pair, FILE *file);
+static void pair_print(const pair_t pair, FILE *file);
 
-// Table functions
-table_t table_create(int initial_capacity){
-    if (initial_capacity <= 0){
+table_t table_create(int initial_capacity) {
+    if (initial_capacity <= 0) {
         fprintf(stderr, "Error: table_create received initial_capacity less than 1\n");
         exit(1);
     }
 
     table_t table = malloc(sizeof(table_impl_t));
-    if (table == NULL){
-        fprintf(stderr, "Error: Memory allocation failed for table\n");
-        exit(1);
-    }
 
     table->capacity = initial_capacity;
     table->size = 0;
     table->pairs = malloc(table->capacity * sizeof(pair_t));
 
-    if (table->pairs == NULL){
-        fprintf(stderr, "Error: Memory allocation failed for table pairs\n");
-        free(table);
-        exit(1);
-    }
-
     return table;
 }
 
-void table_free(table_t table){
-    for (int i = 0; i < table->size; i++){
+void table_free(table_t table) {
+    for (int i = 0; i < table->size; i++) {
         free(table->pairs[i].key);
     }
+
     free(table->pairs);
     free(table);
 }
 
-void table_print(table_t table, FILE *file){
+void table_print(const table_t table, FILE *file) {
     fprintf(file, "\n--- Printing contents of table (Size: %d /Capacity: %d) ---\n", table->size, table->capacity);
-    for(int i = 0; i < table->size; i++){
+
+    for(int i = 0; i < table->size; i++) {
         pair_print(table->pairs[i], file);
     }
 }
 
-void table_set(table_t table, const char *key, int value){
-    // Existing key
-    for (int i = 0; i < table->size; i++){
-        if (strcmp(table->pairs[i].key, key) == 0){
+void table_set(table_t table, const char *key, int value) {
+    for (int i = 0; i < table->size; i++) {
+        if (strcmp(table->pairs[i].key, key) == 0) {
             table->pairs[i].value = value;
             return;
         }
     }
-    // New key
-    if (table->size == table->capacity){
-        // Reallocate memory
+
+    if (table->size == table->capacity) {
         table->capacity *= 2;
-        pair_t *new_table = realloc(table->pairs, table->capacity * sizeof(pair_t));
-        if (new_table == NULL){
-            fprintf(stderr, "Error: Out of memory\n");
-            table_free(table);
-            exit(1);
-        }
-        table->pairs = new_table;
+        table->pairs = realloc(table->pairs, table->capacity * sizeof(pair_t));
     }
-    // Add pair to table
+
     table->pairs[table->size] = pair_create(key, value);
     table->size++;
 }
 
-int table_get(const table_t table, const char *key, int *value){
-    for (int i = 0; i < table->size; i++){
-        if (strcmp(table->pairs[i].key, key) == 0){
-            if (value != NULL){
+int table_get(const table_t table, const char *key, int *value) {
+    for (int i = 0; i < table->size; i++) {
+        if (strcmp(table->pairs[i].key, key) == 0) {
+            if (value != NULL) {
                 *value = table->pairs[i].value;
             }
-            return 1; // Key esists
+
+            return 1;
         }
     }
-    return 0; // Key does not exist
+
+    return 0;
 }
 
-// Pair functions
 static pair_t pair_create(const char *key, int value){
     pair_t pair;
+
     pair.key = strdup(key);
     pair.value = value;
+
     return pair;
 }
 
-static void pair_print(pair_t pair, FILE *file){
+static void pair_print(const pair_t pair, FILE *file){
     fprintf(file, "%s | %d\n", pair.key, pair.value);
 }
